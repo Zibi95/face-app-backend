7b4ec1c27d312ea840081be810386a4868c1d9cb
Zibi95 <sylwester.sob95@gmail.com>
2023-02-06 19:59:31 +0100

    database v9

--- functions/register.js
+++ functions/register.js
@@ -1,25 +1,26 @@
 const handleRegister = (db, bcrypt) => (req, res) => {
   const { email, password, name } = req.body;
 
-  if (!email || !password || !name) return res.json('Not enough data');
+  if (email && password && name) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
 
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(password, salt);
-  db.transaction(trx => {
-    return trx('login')
-      .insert({ email: email, hash: hash })
-      .catch(err => res.json(err))
-      .then(() => {
-        return trx('users').returning('*').insert({
-          name: name,
-          email: email,
-          joined: new Date(),
+    db.transaction(trx => {
+      return trx('login')
+        .insert({ email: email, hash: hash })
+        .then(() => {
+          return trx('users').returning('*').insert({
+            name: name,
+            email: email,
+            joined: new Date(),
+          });
         });
-      });
-  })
-    .catch(err => res.json('Email already used'))
-    .then(user => res.json(user))
-    .catch(err => res.json('Something went wrong'));
+    })
+      .then(user => res.json(user))
+      .catch(err => res.status(400).json('Something went wrong'));
+  } else {
+    res.json('Not enough data');
+  }
 };
 
 module.exports = {
