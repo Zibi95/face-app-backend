fc1bc42ebe6c49624578352ec05499290fb83dc3
Zibi95 <sylwester.sob95@gmail.com>
2023-02-06 19:31:24 +0100

    database v6

--- functions/signin.js
+++ functions/signin.js
@@ -1,21 +1,20 @@
 const handleSignIn = (db, bcrypt) => (req, res) => {
   const { email, password } = req.body;
 
-  if (!email || !password) {
-    return res.status(400).json()('Not enough data');
+  if (email && password) {
+    db.select('*')
+      .from('login')
+      .where('email', '=', email)
+      .then(user => {
+        const hash = user[0]['hash'];
+        const isPasswordValid = bcrypt.compareSync(password, hash);
+        return isPasswordValid && db.select('*').from('users').where('email', '=', email);
+      })
+      .then(data => res.json(data))
+      .catch(err => res.status(400).json('Wrong credentials'));
+  } else {
+    return res.json('Not enough data');
   }
-
-  db.select('*')
-    .from('login')
-    .where('email', '=', email)
-    .then(user => {
-      const hash = user[0]['hash'];
-      const isPasswordValid = bcrypt.compareSync(password, hash);
-      return isPasswordValid ? db.select('*').from('users').where('email', '=', email) : "User and password doesn't match";
-    })
-    .catch(err => res.json(err))
-    .then(data => res.json(data))
-    .catch(err => res.json('Unable to signin'));
 };
 
 module.exports = {
